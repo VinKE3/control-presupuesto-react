940ff2835159b941f45fd54ee577dbd30cfa4161
VinKE3 <harquin09@gmail.com>
2021-12-27 19:20:26 -0300

    commit1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      Diseñado por Kevin Muñoz, todos los derechos reservados 2021.
+    </footer>
+  );
+};
+
+export default Footer;
